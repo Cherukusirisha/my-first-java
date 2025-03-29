dd386e729b20c03c30bb359b9e1c46e5bf4295a3
Cherukusirisha <cherukushirishareddy@gmail.com>
2025-03-29 18:44:44 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1> my heading</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit, labore!</p>
+    </div>
+  )
+}
+export default App;
